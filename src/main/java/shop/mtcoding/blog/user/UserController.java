81a0e52c28ae0416f3db4082af892743ddf5dcd0
seoldonghun <ehdgns5647@gmail.com>
2024-06-03 15:30:43 +0900

    카카오 로그인 완료

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -3,10 +3,7 @@ package shop.mtcoding.blog.user;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.blog._core.utils.ApiUtil;
 
 
@@ -17,6 +14,21 @@ public class UserController {
     private final UserService userService;
     private final HttpSession session;
 
+    // http://ip주소:8080/oauth/callback?accessToken=hrt43uoihf8isrd
+    @GetMapping("/oauth/callback")
+    public ResponseEntity<?> oauthCallback(@RequestParam("accessToken") String kakaoAccessToken) {
+        System.out.println("스프링에서 받은 카카오 토큰 : " + kakaoAccessToken);
+
+        String blogAccessToken = userService.카카오로그인(kakaoAccessToken);
+        // 1. 카카오에서 사용자 정보 요청하기
+
+        // 2. code 방식과 동일
+
+        // 3. jwt(스프링서버) 생성해서 앱에게 전달
+
+        return ResponseEntity.ok().header("Authorization", "Bearer " + blogAccessToken).body(new ApiUtil(null));
+    }
+
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO reqDTO) {
         UserResponse.DTO respDTO = userService.회원가입(reqDTO);
@@ -26,7 +38,7 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserRequest.LoginDTO reqDTO) {
         String jwt = userService.로그인(reqDTO);
-        return ResponseEntity.ok().header("Authorization", "Bearer "+jwt).body(new ApiUtil(null));
+        return ResponseEntity.ok().header("Authorization", "Bearer " + jwt).body(new ApiUtil(null));
     }
 
     @GetMapping("/logout")
