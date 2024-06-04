a51e5a41a61fb3a6a8fdce50759736d278ffd1de
seoldonghun <ehdgns5647@gmail.com>
2024-06-04 16:29:12 +0900

    naver 로그인 크리덴셜 방식

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -14,12 +14,27 @@ public class UserController {
     private final UserService userService;
     private final HttpSession session;
 
-    // http://ip주소:8080/oauth/callback?accessToken=hrt43uoihf8isrd
-    @GetMapping("/oauth/callback")
-    public ResponseEntity<?> oauthCallback(@RequestParam("accessToken") String kakaoAccessToken) {
+    // http://ip주소:8080/oauth/kakao/callback?accessToken=hrt43uoihf8isrd
+    @GetMapping("/oauth/kakao/callback")
+    public ResponseEntity<?> oauthKakaoCallback(@RequestParam("accessToken") String kakaoAccessToken) {
         System.out.println("스프링에서 받은 카카오 토큰 : " + kakaoAccessToken);
 
         String blogAccessToken = userService.카카오로그인(kakaoAccessToken);
+        // 1. 카카오에서 사용자 정보 요청하기
+
+        // 2. code 방식과 동일
+
+        // 3. jwt(스프링서버) 생성해서 앱에게 전달
+
+        return ResponseEntity.ok().header("Authorization", "Bearer " + blogAccessToken).body(new ApiUtil(null));
+    }
+
+    // http://ip주소:8080/oauth/kakao/callback?accessToken=hrt43uoihf8isrd
+    @GetMapping("/oauth/naver/callback")
+    public ResponseEntity<?> oauthNaverCallback(@RequestParam("accessToken") String naverAccessToken) {
+        System.out.println("스프링에서 받은 네이버 토큰 : " + naverAccessToken);
+
+        String blogAccessToken = userService.네이버로그인(naverAccessToken);
         // 1. 카카오에서 사용자 정보 요청하기
 
         // 2. code 방식과 동일
